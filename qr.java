90ec357c70450c5c84c0d8fd44dd2d0627ae22bf
Emile-avd <denyscmll@gmail.com>
2026-06-12 09:15:56 -0600

    Agregamos codigo a qr

--- qr.java
+++ qr.java
@@ -1,3 +1,17 @@
 public class qr {
     
+    private String codigoQr;
+
+    public qr(String codigoQr){
+        this.codigoQr = codigoQr;
+    }
+
+    public String getCodigoQr() {
+        return codigoQr;
+    }
+
+    public boolean validarQr(String codigoIngresado) {
+        return codigoQr.equals(codigoIngresado);
+    }
+
 }
