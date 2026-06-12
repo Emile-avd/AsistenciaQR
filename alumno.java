7ae2c2267939742503dd32bf0780ccc8e95f16b6
Emile-avd <denyscmll@gmail.com>
2026-06-12 08:17:10 -0600

    agregamos codigo a alumno y docente junto con sus respectivos metodos

--- alumno.java
+++ alumno.java
@@ -1,3 +1,19 @@
-public class alumno {
+public class alumno extends usuario{
     
+    private String matricula;
+    private int qr;
+
+    public alumno(String nombre, String rol, String matricula, int qr){
+        super(nombre, rol);
+        this.matricula = matricula;
+        this.qr = qr;
+    }
+
+    public String getMatricula(){
+        return matricula;
+    }
+
+    public int getQR(){
+        return qr;
+    }
 }
--- docente.java
+++ docente.java
@@ -1,3 +1,38 @@
-public class docente {
-    
+public class docente extends usuario{
+
+    private int NumeroEmpleado;
+
+    public docente(String nombre, String rol, int NumeroEmpleado){
+        super(nombre, rol);
+        this.NumeroEmpleado=NumeroEmpleado;
+    }
+
+    public int getNumeroEmpleado(){
+        return NumeroEmpleado;
+    }
+
 }
+
+/*  
+//metodo con simple
+    public void metodoSimple(){
+        //Instrucciones
+    }
+
+    //metodo con parametros
+    public void metodoConParametros(int numero){
+        //Instrucciones
+    } 
+
+    //funcion simple
+    protected int funcionSimple(){
+        //Instrucciones
+        return 0;
+    }
+
+    //funcion con parametros
+    public String funcionConParametros(int numero){
+        //Instrucciones
+        return "";
+    }
+*/
