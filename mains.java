a0c64bb31ff2d561605bcd8b0559f6b3c0fe72ee
Emile-avd <denyscmll@gmail.com>
2026-06-12 08:20:00 -0600

    agregamos codigo a mains

--- mains.java
+++ mains.java
@@ -1,10 +1,21 @@
 public class mains {
     public static void main(String[] args){
-        usuario user1 = new usuario("Juan", "Admin");
-        user1.getNombre();
-        System.out.println(user1.getNombre());
-        user1.getRol();
-        System.out.println(user1.getRol());
+        docente doc1 = new docente("Cui", "Maistro", 1);
+        doc1.getNumeroEmpleado();
+        System.out.println(doc1.getNumeroEmpleado());
+        doc1.getNombre();
+        System.out.println(doc1.getNombre());
+        doc1.getRol();
+        System.out.println(doc1.getRol());
 
+        alumno alumno1 = new alumno("Juanito", "chalan", "2530831", 899846);
+        alumno1.getMatricula();
+        System.out.println(alumno1.getMatricula());
+        alumno1.getNombre();
+        System.out.println(alumno1.getNombre());
+        alumno1.getQR();
+        System.out.println(alumno1.getQR());
+        alumno1.getRol();
+        System.out.println(alumno1.getRol());
     }
 }
