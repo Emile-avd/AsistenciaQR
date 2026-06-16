7d8ea627cd2813bd4cc19123f74c61ec463d0374
Emile-avd <denyscmll@gmail.com>
2026-06-16 08:43:38 -0600

    Cambiamos fecha por new date para inicializar la variable

--- asistencia.java
+++ asistencia.java
@@ -1,3 +1,15 @@
+import java.util.Date;
+
 public  class asistencia {
-    
+    private alumno nombreAlumno;
+    private Date fecha;
+
+    public asistencia(alumno nombreAlumno, Date fecha) {
+        this.nombreAlumno = nombreAlumno;
+        this.fecha = new Date();
+    }
+
+    public void mostrarAsistencia(){
+        System.out.println(nombreAlumno.getNombre() + " " + nombreAlumno.getMatricula() + " " + fecha);
+    }
 }
